84394d0218d68064f8ea0e607bbc0a8e684d4928
Rozen Raymond <zen.revilene@gmail.com>
2020-03-02 23:54:54 +1100

    create all directories and download all s3 files to relevant path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as AWS from "aws-sdk";
 import * as fs from "fs";
+import * as BluebirdPromise from "bluebird";
 
 // List all objects
 // Download objects from a bucket
@@ -11,46 +12,105 @@ import * as fs from "fs";
 //   Encrypt the list into a file using AWS KMS with CMK (master key)
 
 const s3 = new AWS.S3();
+const bucketName = "test-bucket-s3";
 const params = {
- Bucket: "test-bucket-s3"
+  Bucket: bucketName
 };
 
-const download = async (s3Objects: AWS.S3.Object[]): Promise<void> => {
- const [item, ...items] = s3Objects;
-
- if (item.Key) {
-  const result = await s3.getObject({ ...params, Key: item.Key }).promise();
-
-  fs.writeFile(item.Key, result.Body, err => {
-   if (err) {
-    throw err;
-   }
-   console.log("File saved. File:", item.Key);
+const createDownloadedS3ObjectList = (list: string[]) => {
+  const content = list.join("\n");
+  fs.writeFile("./downloaded.txt", content, err => {
+    if (err) throw err;
+    console.log("Downloaded list of S3 object created,");
   });
- }
-
- console.log("S3 download complete");
 };
 
-const listAllObjects = async (): Promise<AWS.S3.Object[]> => {
- try {
-  const response = await s3.listObjectsV2(params).promise();
+const createDirectory = (dirPath: string) => {
+  const directoryPath = `${process.cwd()}/${bucketName}/${dirPath}`;
+  fs.mkdir(directoryPath, { recursive: true }, err => {
+    if (err) throw err;
+    console.log("directory created at", directoryPath);
+  });
+};
 
-  const contents: AWS.S3.Object[] = response.Contents || [];
+const createAllDirectories = (directories: string[]) => {
+  // create all the directories within the s3 bucket
+  directories.forEach(createDirectory);
+};
 
-  if (response.IsTruncated) {
-   const result = await listAllObjects();
-   return [...contents, ...result];
+const download = async (s3Keys: string[]): Promise<void> => {
+  await BluebirdPromise.Promise.map(s3Keys, async (key: string) => {
+    const result = await s3.getObject({ ...params, Key: key }).promise();
+    const filePath = `${process.cwd()}/${bucketName}/${key}`;
+    fs.writeFile(filePath, result.Body, err => {
+      if (err) {
+        throw err;
+      }
+      console.log("File saved. File:", key);
+    });
+  });
+  console.log("S3 download complete");
+};
+
+const getS3ObjectsList = async (): Promise<AWS.S3.Object[]> => {
+  try {
+    const response = await s3.listObjectsV2(params).promise();
+    const contents: AWS.S3.Object[] = response.Contents || [];
+
+    console.log("response", response);
+
+    if (response.IsTruncated) {
+      const result = await getS3ObjectsList();
+      return [...contents, ...result];
+    }
+    return contents;
+  } catch (e) {
+    console.error("Error:", e.message);
+    return [];
   }
-
-  return contents;
- } catch (e) {
-  console.error("Error:", e.message);
-  return [];
- }
 };
 
-// Run download
+interface s3KeysAndDirectories {
+  s3Keys: string[];
+  directories: string[];
+}
+
+const getS3KeysAndDirectories = (
+  s3ListObjects: AWS.S3.Object[]
+): s3KeysAndDirectories => {
+  return s3ListObjects.reduce(
+    (
+      acc: { s3Keys: string[]; directories: string[] },
+      currentObject: AWS.S3.Object
+    ) => {
+      const { s3Keys, directories } = acc;
+
+      const currentObjectKey = currentObject.Key;
+      if (currentObjectKey) {
+        if (currentObjectKey.endsWith("/")) {
+          directories.push(currentObjectKey);
+        } else {
+          s3Keys.push(currentObjectKey);
+        }
+      }
+      return {
+        s3Keys,
+        directories
+      };
+    },
+    { s3Keys: [], directories: [] }
+  );
+};
+
 (async () => {
- await download(await listAllObjects());
+  // get the list of objects from the s3 bucket
+  const s3ListObjects = await getS3ObjectsList();
+  const { s3Keys, directories } = getS3KeysAndDirectories(s3ListObjects);
+  // create directories
+  createAllDirectories(directories);
+
+  // download all s3 objects
+  await download(s3Keys);
+
+  createDownloadedS3ObjectList(s3Keys);
 })();
